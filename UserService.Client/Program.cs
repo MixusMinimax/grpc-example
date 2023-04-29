850a92ae423d658552bf95a2cbece33d949cc1b2
Maxi Barmetler <maxi.barmetler@gmail.com>
2023-04-29 22:02:19 +0200

    asd

---
+++ UserService.Client/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using Proto.User;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var stub = new Proto.User.UserService.UserServiceClient(channel);
+var reply = await stub.SignInAsync(new SignInRequest
+{
+    Username = "maxi.barmetler@gmail.com",
+    Password = "test1234"
+});
+Console.WriteLine($"Response: {reply}");
